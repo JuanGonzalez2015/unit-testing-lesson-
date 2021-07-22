8d29e2b7ccf5ddac320830b14e54445eaa011df0
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-07-22 15:24:40 -0500

    testing

--- src/main/java/CarStuff/CarUtils.java
+++ src/main/java/CarStuff/CarUtils.java
@@ -1,7 +1,9 @@
 package CarStuff;
 
+import java.util.Locale;
+
 public class CarUtils {
-    public static String getCarDetails(Car car){
+    public static String getCarDetails(Car car) {
 
         String make = car.getMake();
         String model = car.getModel();
@@ -10,19 +12,26 @@ public class CarUtils {
         Double mileage = car.getMileage();
         Boolean isElectric = car.isElectric();
 
-        if(car.getMake() == null || car.getModel()== null || car.getYear() == null){
+        if (car.getMake() == null || car.getModel() == null || car.getYear() == null) {
             return "Not able to provide you with details ";
         }
 
-        return  car.getYear() + " " + car.getMake() + " " + car.getModel() + " " + car.getColor();
+        return car.getYear() + " " + car.getMake() + " " + car.getModel() + " " + car.getColor();
     }
 
     //In CarUtils, write a static void method called addToMileage. This method takes in a car object and a double 'mileageToAdd'
     // The method will set the car's mileage to be equal to the current mileage + the mileageToAdd
-    public static void addToMileage(){
-
+    public static void addToMileage(Car car, double mileageToAdd) {
+        double mileage = car.getMileage() + mileageToAdd;
+        System.out.println(mileage);
     }
 
     //Then, write a test, following Arrange/Act/Assert which tests whether the new mileage was correctly added.
     // You will probably call car.getMileage in your assertion and compare it to what you expect.
+
+    public static void convertToElectric(Car car, String engineType) {
+        if (engineType.toLowerCase().contains("electric")) {
+            car.setElectric(true);
+        }
+    }
 }
--- src/test/java/CarDetailsTests.java
+++ src/test/java/CarDetailsTests.java
@@ -56,7 +56,7 @@ public class CarDetailsTests {
     }
 
     @Test
-    public void getCarDetails_GivenThreeNullValues_ReturnsErrorString() {
+    public void getCarDetails_GivenColor_ReturnsErrorString() {
 
         //Arrange
         Car car = new Car();
@@ -72,4 +72,63 @@ public class CarDetailsTests {
         ///Assert
         Assert.assertEquals("2019 Ford Mustang Black", details);
     }
+
+
+    @Test
+    public void getCarDetails_GivenMileage_ReturnsMakeModelYearMileage() {
+
+        //Arrange
+        Car car = new Car();
+        car.setMake("Ford");
+        car.setModel("Mustang");
+        car.setYear("2019");
+        car.setColor("Black");
+        car.setMileage(4523);
+
+
+        //Act
+        String details = CarUtils.getCarDetails(car);
+
+        ///Assert
+        Assert.assertEquals(4523,4523);
+    }
+    @Test
+    public void getCarDetails_GivenElectric_ReturnsElectricTrue() {
+
+        //Arrange
+        Car car = new Car();
+
+        //Act
+        CarUtils.convertToElectric(car, "electric");
+
+        ///Assert
+        Assert.assertTrue(car.isElectric());
+    }
+//    @Test
+//    public void getCarDetails_GivenElectric_ReturnsElectricFalse() {
+//
+//        //Arrange
+//        Car car = new Car();
+//
+//        //Act
+//        CarUtils.convertToElectric(car, " ");
+//
+//        ///Assert
+//        Assert.assertEquals(false, car.isElectric());
+//    }
+
+        @Test
+    public void getCarDetails_GivenElectric_ReturnsElectricLowerCase() {
+
+        //Arrange
+        Car car = new Car();
+
+        //Act
+        CarUtils.convertToElectric(car, "ELECTRIC");
+
+        ///Assert
+        Assert.assertEquals(false, car.isElectric());
+    }
+
+
 }
